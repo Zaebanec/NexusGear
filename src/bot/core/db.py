93f40ec17c2e4eb546c279a32e46cc70f869d71e
Zaebanec <andreymolot51@icloud..com>
2025-07-17 20:43:23 +0300

    Clear and restart

--- src/bot/core/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from .config import settings
-
-# Создаем асинхронный движок SQLAlchemy
-engine = create_async_engine(settings.database_url_asyncpg)
-
-# Создаем фабрику асинхронных сессий
-async_session_factory = async_sessionmaker(engine, expire_on_commit=False)
--- src/bot/models/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.types import BigInteger
-
-class Base(DeclarativeBase):
-    """Base class for all SQLAlchemy models."""
-    id: Mapped[int] = mapped_column(
-        BigInteger, primary_key=True, autoincrement=True
-    )
